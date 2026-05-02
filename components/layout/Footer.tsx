37c51d068c5bbc0dd54c3344289225fbc0548c91
SanderTorg <santorgersen@gmail.com>
2026-05-02 23:46:50 +0200

    added paragraf in footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
     <footer className="bg-gray-800 text-white py-4">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} Holidaze. All rights reserved.</p>
+        <p>This is a student project.</p>
       </div>
     </footer>
   );
