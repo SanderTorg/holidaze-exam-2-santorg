44a4150586e7fc1386f6ada75e61fe8da915e8e6
SanderTorg <santorgersen@gmail.com>
2026-05-12 11:28:50 +0200

    Wrong name for filtering function

--- components/pages/venues/ListingsClient.tsx
+++ components/pages/venues/ListingsClient.tsx
@@ -54,7 +54,7 @@ export function ListingsClient({ venues }: { venues: Venue[] }) {
       )}
 
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {filtered.map((venue: Datum) => (
+        {filtered.map((venue: Venue) => (
           <Card
             key={venue.id}
             className="overflow-hidden flex flex-col shadow-md hover:shadow-xl transition-shadow duration-300 cursor-pointer group p-0"
